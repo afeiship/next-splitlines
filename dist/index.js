cb262039bf8aec5d639ddeb1afcfad99ad67ac8b
afeiship <1290657123@qq.com>
2021-01-09 15:41:00 +0800

    fix: global ctx error with typeof

--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,15 @@
  * name: @jswork/next-splitlines
  * description: Split string by `
 ` or `
-` or `` for next.
+` or `
+` for next.
  * homepage: https://github.com/afeiship/next-splitlines
  * version: 1.0.0
  * date: 2020-11-25 13:30:18
  * license: MIT
  */
 
-(function () {
+(fvar global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
 
